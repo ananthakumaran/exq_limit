760c24840446d0d99108493eac5ff04b94972849
Anantha Kumaran <ananthakumaran@gmail.com>
2020-07-10 19:45:44 +0530

    fix version dependency

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExqLimit.MixProject do
     [
       app: :exq_limit,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps()

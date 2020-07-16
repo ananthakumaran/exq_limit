8740c5814fd9adbc5e6016375c38d616c8f40c04
Anantha Kumaran <ananthakumaran@gmail.com>
2020-07-16 22:16:25 +0530

    add ability to short circuit

--- lib/exq_limit/all.ex
+++ lib/exq_limit/all.ex
@@ -2,11 +2,13 @@ defmodule ExqLimit.All do
   @behaviour Exq.Dequeue.Behaviour
 
   @impl true
-  def init(queue_info, limits) when is_list(limits) and length(limits) > 0 do
+  def init(queue_info, specs) when is_list(specs) and length(specs) > 0 do
     state =
-      Enum.map(limits, fn {module, options} ->
-        {:ok, limit_state} = apply(module, :init, [queue_info, options])
-        {module, limit_state}
+      Enum.map(specs, fn spec ->
+        {module, limit_options, options} = normalize_spec(spec)
+        short_circuit? = Keyword.get(options, :short_circuit, false)
+        {:ok, limit_state} = apply(module, :init, [queue_info, limit_options])
+        {module, limit_state, short_circuit?}
       end)
 
     {:ok, state}
@@ -14,7 +16,7 @@ defmodule ExqLimit.All do
 
   @impl true
   def stop(state) do
-    Enum.each(state, fn {module, limit_state} ->
+    Enum.each(state, fn {module, limit_state, _} ->
       :ok = apply(module, :stop, [limit_state])
     end)
 
@@ -24,9 +26,13 @@ defmodule ExqLimit.All do
   @impl true
   def available?(state) do
     {state, available?} =
-      Enum.reduce(state, {[], true}, fn {module, limit_state}, {state, acc} ->
-        {:ok, available?, limit_state} = apply(module, :available?, [limit_state])
-        {[{module, limit_state} | state], available? && acc}
+      Enum.reduce(state, {[], true}, fn {module, limit_state, short_circuit?}, {state, acc} ->
+        if short_circuit? && !acc do
+          {[{module, limit_state, short_circuit?} | state], acc}
+        else
+          {:ok, available?, limit_state} = apply(module, :available?, [limit_state])
+          {[{module, limit_state, short_circuit?} | state], available? && acc}
+        end
       end)
 
     {:ok, available?, Enum.reverse(state)}
@@ -35,9 +41,9 @@ defmodule ExqLimit.All do
   @impl true
   def dispatched(state) do
     state =
-      Enum.map(state, fn {module, limit_state} ->
+      Enum.map(state, fn {module, limit_state, short_circuit?} ->
         {:ok, limit_state} = apply(module, :dispatched, [limit_state])
-        {module, limit_state}
+        {module, limit_state, short_circuit?}
       end)
 
     {:ok, state}
@@ -46,9 +52,9 @@ defmodule ExqLimit.All do
   @impl true
   def processed(state) do
     state =
-      Enum.map(state, fn {module, limit_state} ->
+      Enum.map(state, fn {module, limit_state, short_circuit?} ->
         {:ok, limit_state} = apply(module, :processed, [limit_state])
-        {module, limit_state}
+        {module, limit_state, short_circuit?}
       end)
 
     {:ok, state}
@@ -57,11 +63,14 @@ defmodule ExqLimit.All do
   @impl true
   def failed(state) do
     state =
-      Enum.map(state, fn {module, limit_state} ->
+      Enum.map(state, fn {module, limit_state, short_circuit?} ->
         {:ok, limit_state} = apply(module, :failed, [limit_state])
-        {module, limit_state}
+        {module, limit_state, short_circuit?}
       end)
 
     {:ok, state}
   end
+
+  defp normalize_spec({module, limit_options}), do: {module, limit_options, []}
+  defp normalize_spec({module, limit_options, options}), do: {module, limit_options, options}
 end
